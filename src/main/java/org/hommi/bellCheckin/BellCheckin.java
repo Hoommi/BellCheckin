9070eeaffbf9c05e8ba1e3531e80f9659b464edc
htilssu <tolashuu@gmail.com>
2025-06-20 12:31:38 +0700

    feat: add configuration and language support for rewards system

--- src/main/java/org/hommi/bellCheckin/BellCheckin.java
+++ src/main/java/org/hommi/bellCheckin/BellCheckin.java
@@ -31,6 +31,10 @@ public final class BellCheckin extends JavaPlugin {
     public void onEnable() {
         BellCheckin.instance = this;
         createDataFolder();
+        // Load config and language files
+        saveDefaultConfig(); // Loads config.yml if not present
+        saveResource("language.yml", false); // Loads language.yml if not present
+        reloadConfig();
         setUpDb();
         this.checkinManager = new CheckinManager();
         this.bellLocationManager = new BellLocationManager();
@@ -59,5 +63,18 @@ public final class BellCheckin extends JavaPlugin {
 
     public BellLocationManager getBellLocationManager() {
         return bellLocationManager;
+    }
+
+    // Load a value from config.yml
+    public Object getRewardConfig(String path) {
+        return getConfig().get(path);
+    }
+
+    // Load a message from language.yml
+    public String getMessage(String path) {
+        // Load language.yml as a YamlConfiguration
+        java.io.File langFile = new java.io.File(getDataFolder(), "language.yml");
+        org.bukkit.configuration.file.YamlConfiguration langConfig = org.bukkit.configuration.file.YamlConfiguration.loadConfiguration(langFile);
+        return langConfig.getString("messages." + path, "Message not found: " + path);
     }
 }
